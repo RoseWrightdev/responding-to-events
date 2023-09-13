6e75b6bc2d22a56ff7dee0e92a4f30d541046498
Rose Wright <rosesophiewright@gmail.com>
2023-09-13 04:51:41 -0500

    backspace

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { sculptureList } from './data.js';
 
 function App() {
+ const [index, setIndex] = useState(0);
+  function handleClickNext() {
+    if(sculptureList.length - 1 === index){
+      setIndex(0)
+    }
+    else{
+      setIndex(index + 1);
+    }
+  }
+
+  function handleClickBack() {
+    setIndex(index - 1);
+  }
+
+  let sculpture = sculptureList[index];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <button onClick={handleClickNext}>
+          Next
+        </button>
+      {index !== 0 ? <button onClick={handleClickBack}>Back</button> : null}
+      <h2>
+        <i>{sculpture.name} </i> 
+        by {sculpture.artist}
+      </h2>
+      <h3>  
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <img 
+        src={sculpture.url} 
+        alt={sculpture.alt}
+      />
+      <p>
+        {sculpture.description}
+      </p>
+    </>
   );
 }
 
